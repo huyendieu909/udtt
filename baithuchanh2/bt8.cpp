26a7a1a8cb79d6f04ca83ff7cadaa919b87f00fc
huyendieu909 <hd911910@gmail.com>
2024-03-28 21:48:59 +0700

    modified:   baithuchanh2/bt8.cpp 	modified:   baithuchanh2/bt8.exe

--- baithuchanh2/bt8.cpp
+++ baithuchanh2/bt8.cpp
@@ -1,5 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
+void show (int O[][9]) {
+    for (int i = 1; i<=8;i++) {
+        for (int j = 1; j<= 8; j++) {
+            cout << O[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n\n";
+}
+bool isValid(int O[][9], int x, int y) {
+    for (int i = 1; i<= x-1;i++) {
+        if (O[i][y] == 1) return false;
+    }
+    int i = 1;
+    while (x - i >= 1 && y-i>=1){
+        if (O[x-i][y-i] == 1) return false;
+        i++;
+    }
+    i=1;
+    while (x-i >= 1 && y+i <= 8) {
+        if (O[x-i][y+i] == 1) return false;
+        i++;
+    }
+    return true;
+}
+void Try(int O[][9], int k){
+    if (k == 9) show(O);
+    for (int i = 1; i <= 8; i++) {
+        if (isValid(O,k,i)){
+            O[k][i] = 1;
+            Try(O, k+1);
+            O[k][i] = 0;
+        }
+    }
+}
 int main() {
-    cout << "2";
+    int O[9][9] = {0};
+    Try(O,1);
 }
